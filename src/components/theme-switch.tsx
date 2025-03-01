0108fdb6c07422e8e172ec72dc4c8a3f1b8d5da3
Faizan Ahmad <85900853+faizi2810@users.noreply.github.com>
2025-03-01 23:27:26 +0500

    Animation added for theme switch button

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -4,6 +4,7 @@ import { useTheme } from "@/context/theme-context";
 import { Button } from "./ui/button";
 import { PiSunLight } from "react-icons/pi";
 import { FaRegMoon } from "react-icons/fa6";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function ThemeSwitch() {
     const { theme, toggleTheme } = useTheme()
@@ -15,7 +16,29 @@ export default function ThemeSwitch() {
                 size='icon'
                 className="fixed bottom-4 right-4 rounded-full shadow-lg h-14 w-14 dark:bg-black dark:text-white"
                 onClick={toggleTheme}>
-                {theme === 'light' ? <PiSunLight size={100} /> : <FaRegMoon size={100} />}
+                <AnimatePresence mode="wait">
+                    {theme === "light" ? (
+                        <motion.div
+                            key="sun"
+                            initial={{ opacity: 0, scale: 0.8, rotate: -90 }}
+                            animate={{ opacity: 1, scale: 1, rotate: 0 }}
+                            exit={{ opacity: 0, scale: 0.8, rotate: 90 }}
+                            transition={{ duration: 0.3, ease: "easeInOut" }}
+                        >
+                            <PiSunLight size={30} />
+                        </motion.div>
+                    ) : (
+                        <motion.div
+                            key="moon"
+                            initial={{ opacity: 0, scale: 0.8, rotate: 90 }}
+                            animate={{ opacity: 1, scale: 1, rotate: 0 }}
+                            exit={{ opacity: 0, scale: 0.8, rotate: -90 }}
+                            transition={{ duration: 0.3, ease: "easeInOut" }}
+                        >
+                            <FaRegMoon size={30} />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </Button>
         </>
     );
