5f742123236fa7365d149e0e910f2186b9bb1e1e
Faizan Ahmad <85900853+faizi2810@users.noreply.github.com>
2025-03-02 22:39:51 +0500

    Active Section Context implemented

---
+++ src/context/active-section-context.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { SectionName } from "@/lib/types";
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from "react";
+
+
+type ActiveSectionContextType = {
+    activeSection: SectionName;
+    setActiveSection: Dispatch<SetStateAction<SectionName>>;
+    timeOfLastClick: number;
+    setTimeOfLastClick: Dispatch<SetStateAction<number>>;
+}
+
+const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
+
+export function ActiveSectionContextProvider({ children }: { children: ReactNode }){
+    const [activeSection, setActiveSection] = useState<SectionName>("Home");
+    const [timeOfLastClick, setTimeOfLastClick] = useState<number>(0);
+    return (
+        <ActiveSectionContext.Provider value={{
+            activeSection, 
+            setActiveSection,
+            timeOfLastClick,
+            setTimeOfLastClick
+            }}>
+            {children}
+        </ActiveSectionContext.Provider>
+    );
+}
+
+export const useActiveSectionContext = () => {
+    const context = useContext(ActiveSectionContext);
+    if (!context) {
+        throw new Error(
+          "useActiveSectionContext must be used within a ActiveSectionContextProvider"
+        );
+      }
+      return context;
+}
