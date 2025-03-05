ee6d781766612d95fb792be51438befff3962c2a
Faizan Ahmad <85900853+faizi2810@users.noreply.github.com>
2025-03-05 22:30:01 +0500

    useSectionInView
    react intersection observer

---
+++ src/lib/hooks.ts
@@ -0,0 +1,21 @@
+import { useInView } from "react-intersection-observer";
+import { SectionName } from "./types";
+import { useEffect } from "react";
+import { useActiveSectionContext } from "@/context/active-section-context";
+
+
+export function useSectionInView(SectionName: SectionName, threshold = 0.75){
+    const { ref, inView } = useInView({
+        threshold,
+    });
+
+    const { timeOfLastClick,setActiveSection } = useActiveSectionContext();
+
+    useEffect(()=>{
+     if(inView && Date.now() - timeOfLastClick > 1000){
+        setActiveSection(SectionName);
+     }   
+    },[SectionName, inView, setActiveSection, timeOfLastClick])
+    
+    return{ref};
+}
