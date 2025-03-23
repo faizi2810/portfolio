986b0488befa372dea3a8babe9be6c963c73aa69
Faizan Ahmad <85900853+faizi2810@users.noreply.github.com>
2025-03-23 16:45:52 +0500

    Skills component and skills data(icons)

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
+
 export const links = [
     {
       name: "Intro",
@@ -23,4 +24,95 @@ export const links = [
       name: "Contact",
       hash: "#contact",
     },
+  ] as const;
+
+  export const skillsData = [
+    {
+      name: ".Net",
+      icon: "skill-icons:dotnet",
+    },
+    {
+      name: "C#",
+      icon: "devicon:csharp",
+    },
+    {
+      name: "JavaScript",
+      icon: "logos:javascript",
+    },
+    {
+      name: "TypeScript",
+      icon: "logos:typescript-icon",
+    },
+    {
+      name: "React",
+      icon: "logos:react",
+    },
+    {
+      name: "Next Js",
+      icon: "devicon:nextjs",
+    },
+    {
+      name: "Angular",
+      icon: "logos:angular-icon",
+    },
+    {
+      name: "Node.js",
+      icon: "logos:nodejs-icon",
+    },
+    {
+      name: "Python",
+      icon: "logos:python",
+    },
+    {
+      name: "Cosmos DB",
+      icon: "devicon:cosmosdb",
+    },
+    {
+      name: "PostgreSQL",
+      icon: "devicon:postgresql",
+    },
+    {
+      name: "MySQL",
+      icon: "logos:mysql",
+    },
+    {
+      name: "HTML",
+      icon: "vscode-icons:file-type-html",
+    },
+    {
+      name: "CSS",
+      icon: "vscode-icons:file-type-css",
+    },
+    {
+      name: "SCSS",
+      icon: "logos:sass",
+    },
+    {
+      name: "Git",
+      icon: "logos:git-icon",
+    },
+    {
+      name: "Bitbucket",
+      icon: "vscode-icons:file-type-bitbucketpipeline",
+    },
+    {
+      name: "Jira",
+      icon: "devicon:jira",
+    },
+    {
+      name: "Confluence",
+      icon: "vscode-icons:file-type-confluence",
+    },
+    {
+      name: "Azure",
+      icon: "devicon:azure",
+    },
+    {
+      name: "AWS",
+      icon: "logos:aws",
+    },
+    {
+      name: "Docker",
+      icon: "logos:docker-icon",
+    }
   ] as const;
